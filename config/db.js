7d3f698296cfc36ceceec2d4baba0abc0d2d6c31
Kishan Kumar <120402685+kishanyadav9199@users.noreply.github.com>
2026-06-26 14:59:04 +0530

    Refactor DB connection to use environment variable
    Updated database connection to use environment variable for URI and added DNS servers.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+
+
+dns.setServers([
+  '1.1.1.1',
+  '8.8.8.8'
+])
  export const connectDB= async()=>{
-    await mongoose
-      .connect(
-        "mongodb+srv://wwwkishan763_db_user:6eXEOPTB1IoyVyG5@cluster0.gko8xrp.mongodb.net/DeliveryApp"
-      )
-      .then(() => {
-        console.log("Database connected");
-      });
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
+      console.log("Database connected");
+    });
 }
 
 
@@ -15,15 +19,6 @@ import mongoose from "mongoose";
 
 
 
-//mongodb+srv://yadavgdatabase:<db_password>@cluster0.sqemgi4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// mongodb+srv://wwwkishan763_db_user:6eXEOPTB1IoyVyG5@cluster0.gko8xrp.mongodb.net/?appName=Cluster0
-
- // "mongodb+srv://wwwkishan763_db_user:6eXEOPTB1IoyVyG5@cluster0.gko8xrp.mongodb.net/food-delivery"
-
-
-
-// NcR0jevP41YuRFFi paswoed
-// wwwkishan763 username
 
 
 
